fc73168777fdaf01276eebde79b6150983e8dc7b
bog_v_nas <lordofgodnd@gmail.com>
2020-05-03 09:12:44 +0300

    finished 7 module

---
+++ src/com/company/gui/panel/StatisticsTypePanel.java
@@ -0,0 +1,30 @@
+package com.company.gui.panel;
+
+import com.company.gui.MainButton;
+import com.company.gui.MainFrame;
+import com.company.settings.HandlerCode;
+import com.company.settings.Text;
+
+import javax.swing.*;
+
+/**
+ * @author N.Petrov
+ * @link http://N.Petrov.com
+ */
+public final class StatisticsTypePanel extends AbstractPanel {
+
+    private final String title;
+
+    public StatisticsTypePanel(MainFrame frame, String title) {
+        super(frame);
+        this.title = Text.get(title);
+        init();
+    }
+
+    @Override
+    protected void init() {
+        MainButton type = new MainButton(title, null, HandlerCode.TYPE);
+        add(type);
+    }
+
+}
